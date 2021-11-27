73a26351a1adbcf2a20f5c72c17d272988918f0d
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-11-27 01:54:36 -0100

    update

--- weather-app/js/weather.js
+++ weather-app/js/weather.js
@@ -12,7 +12,7 @@ weather.temperature = {
 }
 
 const KELVIN = 273
-const key = ''
+const key = '96f6ba150dbfde1782395bffef95d6a2'
 
 if('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(setPosition, showError)
@@ -32,9 +32,31 @@ function setPosition(position) {
 
 
 function getWeather(latitude, longitude) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
     
+    fetch(api).then(function(response) {
+        let data = response.json()
+        return data
+    })
+    .then(function(data){
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN)
+        weather.description = data.weather[0].description
+        weather.iconId = data.weather[0].icon
+        weather.city = data.name
+        weather.country = data.sys.country
+    })
+    .then(function(){
+        displayWeather();
+    })
 }
 
+
+function displayWeather(){
+    iconElement.innerHTML = `<img src="img/openweather-icons/${weather.iconId}.png" class="img-fluid" alt="icon">`
+    temperatureElement.innerHTML = `${weather.temperature.value}`
+    descriptionElement.innerHTML = weather.description
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`
+}
 
 function showError(error){
     notificationElement.style.display = 'block'
