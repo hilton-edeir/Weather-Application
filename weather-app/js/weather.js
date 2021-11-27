9f85c6f683e41ed85e3ff67a001d11a8c3ed9c59
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-11-27 00:44:23 -0100

    upddate

--- weather-app/js/weather.js
+++ weather-app/js/weather.js
@@ -1,5 +1,42 @@
 const iconElement = document.querySelector('#icon-temperature')
 const temperatureElement = document.querySelector('#temperature-value')
-const descriptionElement = document.querySelector('')
+const descriptionElement = document.querySelector('#weather-description')
 const locationElement = document.querySelector('#location')
-const notificationElement = document.querySelector('')
+const notificationElement = document.querySelector('#notification')
+
+
+const weather = {}
+
+weather.temperature = {
+    unit: 'celsius'
+}
+
+const KELVIN = 273
+const key = ''
+
+if('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError)
+}
+else{
+    notificationElement.style.display = 'block'
+    notificationElement.innerHTML = `<div class="alert alert-danger" role="alert">Browser doesn't support Geolocalization</div>`
+}
+
+
+function setPosition(position) {
+    let latitude = position.coords.latitude
+    let longitude =  position.coords.longitude
+    
+    getWeather(latitude, longitude)
+}
+
+
+function getWeather(latitude, longitude) {
+    
+}
+
+
+function showError(error){
+    notificationElement.style.display = 'block'
+    notificationElement.innerHTML = ` ${error.message}`
+}
